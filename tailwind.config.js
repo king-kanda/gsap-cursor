968a1c83b0890d0bb2416054a00eef3ae8836353
king-kand <stevenokanda@gmail.com>
2022-05-25 16:34:30 +0300

    custom cursors and GSAP SAND BOX

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./styles/**/*.{html,js}",
+            "./*.html"],
+  theme: {
+    screen : {
+      sm : '480px',
+      md : '780px',
+      lg : '976px',
+      xl : '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
